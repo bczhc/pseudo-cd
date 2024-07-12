48c09066e4fc769c39fa8a7742de158251ec1dbb
Zhai Can <bczhc0@126.com>
2024-07-12 17:54:12 +0800

    Revert "playback: use unlimited channel for PlayerResult"
    This reverts commit 9fbe9730c4ff229b872341cb963e92cd89e734d2.
    
    Strictly, every player response should be consumed. This is not a concern.

--- src/playback.rs
+++ src/playback.rs
@@ -133,23 +133,7 @@ impl PlaybackHandle {
     }
 
     pub fn player_result(&self) -> PlayerResult {
-        let rx = mutex_lock!(self.result_rx);
-        let mut last = rx.recv().unwrap();
-        // if multiple pending values are present, take the last one
-        loop {
-            match rx.try_recv() {
-                Ok(value) => {
-                    last = value;
-                }
-                Err(TryRecvError::Empty) => {
-                    break;
-                }
-                Err(e) => {
-                    panic!("{}", e);
-                }
-            }
-        }
-        last
+        mutex_lock!(self.result_rx).recv().unwrap()
     }
 
     pub fn send_recv(&self, cmd: PlayerCommand) -> PlayerResult {
@@ -170,7 +154,7 @@ pub fn start_global_playback_thread<D, F>(
     D: Send + 'static,
     F: Fn(PlayerCallbackEvent, &D) + Send + 'static {
     let (cmd_tx, cmd_rx) = sync_channel::<PlayerCommand>(1);
-    let (result_tx, result_rx) = channel::<PlayerResult>();
+    let (result_tx, result_rx) = sync_channel::<PlayerResult>(1);
     let result_rx = Arc::new(Mutex::new(result_rx));
     
     let (stream, sample_tx) = create_audio_stream()?;
